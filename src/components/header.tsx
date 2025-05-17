1d4b29c31a9c1e3270004930a076489f9ced6cfa
Pascal Ordano <pascalordanodev@gmail.com>
2025-05-17 09:28:27 -0300

    branch test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        Rizo
+        Rizo TEST TEST TEST
       </h1>
       <p className="mt-3 text-lg leading-8 text-gray-600">
         Stay updated with your favorite content in one place
